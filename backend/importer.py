b40271fd4d2564adcaa587b27575aa855f77c5dc
Christoph Walcher <christoph-wa@gmx.de>
2020-03-21 23:42:42 +0100

    added importer
    test only

---
+++ backend/importer.py
@@ -0,0 +1,20 @@
+import csv
+import urllib.request
+import json      
+
+
+with open('/Users/christophwalcher/Weinretter.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    next(reader, None)  # skip the header
+    for r in reader:
+        if not r[4]:
+            continue
+        print(r)
+        req = urllib.request.Request("https://weinretter.de/api/restaurant")
+        req.add_header('Content-Type', 'application/json; charset=utf-8')
+        body = {"name": r[1], "link": r[0], "location": {"lat": float(r[3]), "lng": float(r[4])}}
+        print(body)
+        jsondata = json.dumps(body).encode('utf-8')   # needs to be bytes
+        req.add_header('Content-Length', len(jsondata))
+        urllib.request.urlopen(req, jsondata)
+
