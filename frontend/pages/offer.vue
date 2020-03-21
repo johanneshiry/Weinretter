b0704cbabcadd46ba65edec1e4e38d8c91c66513
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-03-21 22:28:57 +0100

    added navigation to offer page

--- frontend/pages/offer.vue
+++ frontend/pages/offer.vue
@@ -1,40 +1,46 @@
 <template>
-  <div class="container">
-    <b-form @submit.prevent="submit">
-      <b-form-group
-        id="input-group-1"
-        label="Name deines Restaurant:"
-        label-for="input-1"
-      >
-        <b-form-input
-          id="input-1"
-          v-model="name"
-          required
-          placeholder="La Pizza"
-        />
-      </b-form-group>
+  <div class="offer">
+    <Navigation></Navigation>
+    <div class="container">
+      <b-form @submit.prevent="submit">
+        <b-form-group
+          id="input-group-1"
+          label="Name deines Restaurant:"
+          label-for="input-1"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="name"
+            required
+            placeholder="La Pizza"
+          />
+        </b-form-group>
 
-      <b-form-group id="input-group-2" label="Website zu deinem Restaurant:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="link"
-          type="url"
-          required
-          placeholder="http://lapizza.de"
-        />
-      </b-form-group>
+        <b-form-group id="input-group-2" label="Website zu deinem Restaurant:" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="link"
+            type="url"
+            required
+            placeholder="http://lapizza.de"
+          />
+        </b-form-group>
 
-      <l-map ref="map" id="mapid" :zoom=7 :center="[51.163375, 10.447683]">
-        <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"/>
-        <l-marker v-if="location" :lat-lng="location"/>
-      </l-map>
-      <b-button type="submit" variant="primary">Registrieren</b-button>
-    </b-form>
+        <l-map ref="map" id="mapid" :zoom=7 :center="[51.163375, 10.447683]">
+          <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"/>
+          <l-marker v-if="location" :lat-lng="location"/>
+        </l-map>
+        <b-button type="submit" variant="primary">Registrieren</b-button>
+      </b-form>
+    </div>
   </div>
 </template>
 
 <script>
   import Vue from 'vue'
+  import Navigation from "../components/Navigation";
+  import Logo from '~/components/Logo.vue'
+
 
   export default Vue.extend({
     data() {
@@ -46,7 +52,7 @@
     },
     methods: {
       async submit() {
-        if(!this.location) {
+        if (!this.location) {
           this.$bvToast.toast('Wähle bitte den Standort deines Restaurant auf der Karte aus', {
             title: 'Fehler',
             autoHideDelay: 5000,
@@ -71,6 +77,10 @@
         title: "Registriere dein Restaurant - WeinRetter",
       }
     },
+    components: {
+      Logo,
+      Navigation
+    }
   })
 </script>
 
