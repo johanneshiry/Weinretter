095e714e7c04ade680113ab1c7cae92e56301dc5
Mathias Arens <mathias@arens.ma>
2020-03-21 13:55:52 +0100

    rough start for two pages

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,73 +1,66 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        weinretter-frontend
-      </h1>
+      <logo/>
+      <p class="title">
+        Rette dein <span class="highlight">Lieblingsrestaurant</span> durch das Kaufen von Wein
+      </p>
       <h2 class="subtitle">
         weinretter
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <nuxt-link to="/map" class="button--green">
+          Karte ansehen
+        </nuxt-link>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
+  import Vue from 'vue'
+  import Logo from '~/components/Logo.vue'
 
-export default Vue.extend({
-  components: {
-    Logo
-  }
-})
+  export default Vue.extend({
+    components: {
+      Logo
+    }
+  })
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .highlight {
+    color: red;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+
+  .links {
+    padding-top: 15px;
+  }
 </style>
