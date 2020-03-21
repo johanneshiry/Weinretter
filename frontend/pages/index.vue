5ac5ef16995ce5abf1af620d6a045a47982cfcff
Mathias Arens <mathias@arens.ma>
2020-03-21 14:32:52 +0100

    Add map page with real leaflet map

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="container">
     <div>
-      <logo/>
+      <logo />
       <p class="title">
-        Rette dein <span class="highlight">Lieblingsrestaurant</span> durch das Kaufen von Wein
+        Rette dein <span class="highlight">Lieblingsrestaurant</span> durch das
+        Kaufen von Wein
       </p>
       <h2 class="subtitle">
         weinretter
@@ -18,49 +19,49 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import Logo from '~/components/Logo.vue'
+import Vue from 'vue'
+import Logo from '~/components/Logo.vue'
 
-  export default Vue.extend({
-    components: {
-      Logo
-    }
-  })
+export default Vue.extend({
+  components: {
+    Logo
+  }
+})
 </script>
 
 <style>
-  .container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
 
-  .title {
-    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
 
-  .highlight {
-    color: #B12525;
-  }
+.highlight {
+  color: #b12525;
+}
 
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
 
-  .links {
-    padding-top: 15px;
-  }
+.links {
+  padding-top: 15px;
+}
 </style>
