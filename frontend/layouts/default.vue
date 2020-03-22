b591c503fa441c55f251052b5c9b4076714da0f8
Mathias Arens <mathias@arens.ma>
2020-03-23 00:08:01 +0100

    Remove some unused code

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -41,7 +41,7 @@
   }
 
   web-info-widget {
-    right:1.5rem;
+    right: 1.5rem;
     bottom: 1.5rem;
     --color-primary: var(--highlight-red);
   }
@@ -51,36 +51,6 @@
   *:after {
     box-sizing: border-box;
     margin: 0;
-  }
-
-
-  .button--green {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #3b8070;
-    color: #3b8070;
-    text-decoration: none;
-    padding: 10px 30px;
-  }
-
-  .button--green:hover {
-    color: #fff;
-    background-color: #3b8070;
-  }
-
-  .button--grey {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #35495e;
-    color: #35495e;
-    text-decoration: none;
-    padding: 10px 30px;
-    margin-left: 15px;
-  }
-
-  .button--grey:hover {
-    color: #fff;
-    background-color: #35495e;
   }
 
   .default-layout {
