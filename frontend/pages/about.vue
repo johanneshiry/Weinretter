797b735f603d578372705ea6191d5c9152fc9f09
Gero Gerke <11deutron11@gmail.com>
2020-03-22 17:08:53 +0100

    Change colors to red in about

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="about">
-    <Navigation/>
+    <Navigation />
     <div class="about">
-      <h2 class="about-title">
-        Über WeinRetter
-      </h2>
+      <h2 class="about-title">Über WeinRetter</h2>
       <p>
         Die COVID-19-Krise trifft insbesondere Restaurants schwer. Durch die behördlich verordneten
         Schließungen oder beschränkten Öffnungszeiten erleiden viele einen erheblichen Umsatzrückgang,
@@ -15,54 +13,70 @@
         verkauft werden, können Restaurants einen Teil ihrer laufenden Kosten decken und ihre wirtschaftliche Lage
         verbessern.
       </p>
+      <p>Trage zum Fortbestand deines Lieblingsrestaurants bei, indem du Wein aus dessen Lagerbestand kaufst!</p>
       <p>
-        Trage zum Fortbestand deines Lieblingsrestaurants bei, indem du Wein aus dessen Lagerbestand kaufst!
-      </p>
-      <p>
-        Diese Seite wurde innerhalb des <a href="https://wirvsvirushackathon.org/" target="_blank">#WirVsVirus
-        Hackathons</a>
-        erstellt und basiert auf dem Vorbild von <a href="https://cellars.nyc" target="_blank">Cellars.NYC</a>.
+        Diese Seite wurde innerhalb des
+        <a href="https://wirvsvirushackathon.org/" target="_blank">
+          #WirVsVirus
+          Hackathons
+        </a>
+        erstellt und basiert auf dem Vorbild von
+        <a
+          href="https://cellars.nyc"
+          target="_blank"
+        >Cellars.NYC</a>.
         Bei Fragen kontaktiere uns einfach auf
-        <a href="https://www.instagram.com/weinretter_de" target="_blank">
-          <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'instagram' }"/>
-          Instagram.</a>
+        <a
+          href="https://www.instagram.com/weinretter_de"
+          target="_blank"
+        >
+          <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'instagram' }" />Instagram.
+        </a>
       </p>
-      <img class="wvvlogo" src="../assets/Logo_WirVsVirus.png" alt="#WirVsVirus Logo"/>
+      <img class="wvvlogo" src="../assets/Logo_WirVsVirus.png" alt="#WirVsVirus Logo" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import Navigation from '../components/Navigation.vue';
+import Vue from 'vue'
+import Navigation from '../components/Navigation.vue'
 
-  export default Vue.extend({
-    components: {
-      Navigation,
-    },
+export default Vue.extend({
+  components: {
+    Navigation
+  },
 
-    head() {
-      return {
-        title: 'Über uns - WeinRetter'
-      };
+  head() {
+    return {
+      title: 'Über uns - WeinRetter'
     }
-  });
+  }
+})
 </script>
 
 <style scoped>
-  .wvvlogo {
-    width: 100%;
-  }
+a {
+  color: var(--highlight-red);
+  text-decoration: none;
+}
+a:hover {
+  color: var(--highlight-red);
+  text-decoration: underline;
+}
+.wvvlogo {
+  width: 100%;
+}
 
-  .about-title {
-    margin-bottom: 0.5em;
-  }
+.about-title {
+  margin-bottom: 0.5em;
+}
 
-  .about {
-    max-width: 45em;
-    width: 90%;
-    margin-left: auto;
-    margin-right: auto;
-    padding: 1em;
-  }
+.about {
+  max-width: 45em;
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 1em;
+}
 </style>
