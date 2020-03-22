d36f59ac65e7cee059c2bf9d8e653b0e590cad52
Mathias Arens <mathias@arens.ma>
2020-03-22 17:00:47 +0100

    Add instagram link to about page text

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -2,28 +2,33 @@
   <div id="about">
     <Navigation/>
     <div class="about">
-    <h2 class="about-title">
-      Über WeinRetter
-    </h2>
-    <p>
-      Die COVID-19-Krise trifft insbesondere Restaurants schwer. Durch die behördlich verordneten
-      Schließungen oder beschränkten Öffnungszeiten erleiden viele einen erheblichen Umsatzrückgang,
-      bis hin zum vollständigen Umsatzausfall.
-    </p>
-    <p>
-      Gleichzeitig verfügen viele Restaurants über ungenutztes Potenzial: volle Weinkeller. Indem diese Bestände
-      verkauft werden, können Restaurants einen Teil ihrer laufenden Kosten decken und ihre wirtschaftliche Lage
-      verbessern.
-    </p>
-    <p>
-      Trage zum Fortbestand deines Lieblingsrestaurants bei, indem du Wein aus dessen Lagerbestand kaufst!
-    </p>
-    <p>
-      Diese Seite wurde innerhalb des <a href="https://wirvsvirushackathon.org/" target="_blank">#WirVsVirus Hackathons</a>
-      erstellt und basiert auf dem Vorbild von <a href="https://cellars.nyc" target="_blank">Cellars.NYC</a>.
-    </p>
+      <h2 class="about-title">
+        Über WeinRetter
+      </h2>
+      <p>
+        Die COVID-19-Krise trifft insbesondere Restaurants schwer. Durch die behördlich verordneten
+        Schließungen oder beschränkten Öffnungszeiten erleiden viele einen erheblichen Umsatzrückgang,
+        bis hin zum vollständigen Umsatzausfall.
+      </p>
+      <p>
+        Gleichzeitig verfügen viele Restaurants über ungenutztes Potenzial: volle Weinkeller. Indem diese Bestände
+        verkauft werden, können Restaurants einen Teil ihrer laufenden Kosten decken und ihre wirtschaftliche Lage
+        verbessern.
+      </p>
+      <p>
+        Trage zum Fortbestand deines Lieblingsrestaurants bei, indem du Wein aus dessen Lagerbestand kaufst!
+      </p>
+      <p>
+        Diese Seite wurde innerhalb des <a href="https://wirvsvirushackathon.org/" target="_blank">#WirVsVirus
+        Hackathons</a>
+        erstellt und basiert auf dem Vorbild von <a href="https://cellars.nyc" target="_blank">Cellars.NYC</a>.
+        Bei Fragen kontaktiere uns einfach auf
+        <a href="https://www.instagram.com/weinretter_de" target="_blank">
+          <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'instagram' }"/>
+          Instagram.</a>
+      </p>
       <img class="wvvlogo" src="../assets/Logo_WirVsVirus.png" alt="#WirVsVirus Logo"/>
-  </div>
+    </div>
   </div>
 </template>
 
