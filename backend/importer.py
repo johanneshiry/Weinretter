f67697ea1d19836d6ae0e051352fa5cd16e7e2e6
Mathias Arens <mathias@arens.ma>
2020-03-24 15:53:20 +0100

    Add route to read one restaurant data.
    Format backend code with black

--- backend/importer.py
+++ backend/importer.py
@@ -1,30 +1,31 @@
 import csv
 import urllib.request
-import json      
+import json
 
 
-with open('/Users/christophwalcher/Weinretter.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+with open("/Users/christophwalcher/Weinretter.csv", newline="") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",", quotechar='"')
     next(reader, None)  # skip the header
     for r in reader:
         if not r[4]:
             continue
         print(r)
         req = urllib.request.Request("https://weinretter.de/api/restaurants")
-        req.add_header('Content-Type', 'application/json; charset=utf-8')
+        req.add_header("Content-Type", "application/json; charset=utf-8")
         address = None
         description = None
         if not r[2] == "":
             address = r[2]
         if not r[5] == "":
             description = r[5]
-        body = {"name": r[1],
-                "link": r[0],
-                "address": address,
-                "location": {"lat": float(r[3]), "lng": float(r[4])},
-                "description": description}
+        body = {
+            "name": r[1],
+            "link": r[0],
+            "address": address,
+            "location": {"lat": float(r[3]), "lng": float(r[4])},
+            "description": description,
+        }
         print(body)
-        jsondata = json.dumps(body).encode('utf-8')   # needs to be bytes
-        req.add_header('Content-Length', len(jsondata))
+        jsondata = json.dumps(body).encode("utf-8")  # needs to be bytes
+        req.add_header("Content-Length", len(jsondata))
         urllib.request.urlopen(req, jsondata)
-
--- backend/validation.py
+++ backend/validation.py
@@ -3,9 +3,11 @@ from schematics.exceptions import ValidationError
 
 
 def validate_captcha(token):
-    r = requests.post('https://www.google.com/recaptcha/api/siteverify',
-                      {'response': token, 'secret': '6Le3Kp4UAAAAAMkPcidI-o8gDu807ZnqzLr9Axqb'})
+    r = requests.post(
+        "https://www.google.com/recaptcha/api/siteverify",
+        {"response": token, "secret": "6Le3Kp4UAAAAAMkPcidI-o8gDu807ZnqzLr9Axqb"},
+    )
     json = r.json()
 
     if not json["success"] or json["score"] < 0.5:
-        raise ValidationError('Captcha invalid')
+        raise ValidationError("Captcha invalid")
