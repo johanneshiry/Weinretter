422ea62e7ad204fe986d775923acde5244192c4c
Mathias Arens <mathias@arens.ma>
2020-03-24 15:54:50 +0100

    Autoformat backend code with black

--- backend/app.py
+++ backend/app.py
@@ -39,7 +39,7 @@ class CreateRestaurantRequest(Model):
     address = ModelType(AddressType, required=True)
 
 
-@app.route('/api/restaurants', methods=['POST'])
+@app.route("/api/restaurants", methods=["POST"])
 def create_restaurant():
     try:
         r = CreateRestaurantRequest(request.get_json())
@@ -47,66 +47,73 @@ def create_restaurant():
     except DataError as e:
         return jsonify(e.to_primitive()), 400
 
-    restaurant = {'link': r.link,
-                  'name': r.name,
-                  'location': (r.location.lng, r.location.lat),
-                  'address': {
-                      'housenumber': r.address.housenumber,
-                      'street': r.address.street,
-                      'plz': r.address.plz,
-                      'city': r.address.city
-                  },
-                  'telephone': r.telephone,
-                  'description': r.description,
-                  'tags': r.tags}
+    restaurant = {
+        "link": r.link,
+        "name": r.name,
+        "location": (r.location.lng, r.location.lat),
+        "address": {
+            "housenumber": r.address.housenumber,
+            "street": r.address.street,
+            "plz": r.address.plz,
+            "city": r.address.city,
+        },
+        "telephone": r.telephone,
+        "description": r.description,
+        "tags": r.tags,
+    }
 
     result = collection.insert_one(restaurant)
     rid = result.inserted_id
     passcode = token_hex(12)
-    passcode_collection.insert_one({'restaurant_id': str(rid), 'passcode': passcode})
+    passcode_collection.insert_one({"restaurant_id": str(rid), "passcode": passcode})
 
     try:
         content_bot.notify(rid=rid, name=r.name, link=r.link)
     except Exception as e:
         print(e)
 
-    return jsonify({'restaurant_id': str(rid), 'passcode': passcode}), 201
+    return jsonify({"restaurant_id": str(rid), "passcode": passcode}), 201
 
 
-@app.route('/api/restaurants')
+@app.route("/api/restaurants")
 def fetch_restaurants():
     try:
-        left_lng = float(request.args.get('left_lng'))
-        right_lng = float(request.args.get('right_lng'))
-        bottom_lat = float(request.args.get('bottom_lat'))
-        top_lat = float(request.args.get('top_lat'))
+        left_lng = float(request.args.get("left_lng"))
+        right_lng = float(request.args.get("right_lng"))
+        bottom_lat = float(request.args.get("bottom_lat"))
+        top_lat = float(request.args.get("top_lat"))
     except ValueError:
-        return '', 400
+        return "", 400
 
-    cursor = collection.find({
-        '$and': [{
-            'location': {
-                '$within': {
-                    '$box': [[left_lng, bottom_lat], [right_lng, top_lat]]
-                }
-            }
-        }, {
-            'blocked': {
-                '$not': {'$eq': True}
-            }
-        }]
-    })
+    cursor = collection.find(
+        {
+            "$and": [
+                {
+                    "location": {
+                        "$within": {
+                            "$box": [[left_lng, bottom_lat], [right_lng, top_lat]]
+                        }
+                    }
+                },
+                {"blocked": {"$not": {"$eq": True}}},
+            ]
+        }
+    )
 
-    restaurants = [{'name': r['name'],
-                    'link': r['link'],
-                    'location': {'lat': r['location'][1], 'lng': r['location'][0]},
-                    'address': r['address'],
-                    'telephone': r.get('telephone'),
-                    'description': r.get('description'),
-                    'tags': r.get('tags')
-                    } for r in cursor]
+    restaurants = [
+        {
+            "name": r["name"],
+            "link": r["link"],
+            "location": {"lat": r["location"][1], "lng": r["location"][0]},
+            "address": r["address"],
+            "telephone": r.get("telephone"),
+            "description": r.get("description"),
+            "tags": r.get("tags"),
+        }
+        for r in cursor
+    ]
     return jsonify(restaurants)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
