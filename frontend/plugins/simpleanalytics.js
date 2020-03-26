d405cb149d5761c249f5dd38d06c8fc62547e9a0
Mathias Arens <tatellos@users.noreply.github.com>
2020-03-26 17:56:44 +0100

    Try fixing the events by using the old method (#34)

--- frontend/plugins/simpleanalytics.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-
-const sa_event = function() {
-  const a = [].slice.call(arguments);
-  sa_event.q ? sa_event.q.push(a) : (sa_event.q = [a]);
-};
-
-Vue.prototype.$sa_event = sa_event;
